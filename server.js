f776cd02ef5ce54f93112a48fdc6fff19b315041
ndamatta <damatta.natanael@gmail.com>
2024-05-07 11:57:06 -0300

    add inventory route

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 
-/* ***********************
- * Routes
- *************************/
+//Routes
 app.use(static)
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+app.use('/inv', inventoryRoute)
 
 //Index route
 app.get('/', baseController.buildHome)
