5c15b2ba8d692ba29576de2968d445c016150d5a
ndamatta <damatta.natanael@gmail.com>
2024-05-13 15:35:53 -0300

    add session and msg

--- server.js
+++ server.js
@@ -6,10 +6,30 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const utilities = require('./utilities/')
+const session = require("express-session")
+const pool = require("./database/")
 
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+
+//Session
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+//Express msgs middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 //Routes
 app.use(static)
