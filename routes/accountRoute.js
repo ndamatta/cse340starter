2e77ad048e4258c281257b2316c3e6ac9c0700ab
ndamatta <damatta.natanael@gmail.com>
2024-05-14 09:06:07 -0300

    add route and export

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,4 +1,11 @@
+//Needed resources
 const express = require("express")
 const router = express.Router()
 const utilities = require("../utilities/")
 const controller = require("../controllers/accountController")
+
+//Routes
+router.get('/login', utilities.handleErrors(controller.buildLogin));
+
+module.exports = router;
+
