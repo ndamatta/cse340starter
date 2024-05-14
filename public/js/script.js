9bafa6f1a9019457867c8ec80e3b58d5c2262a21
ndamatta <damatta.natanael@gmail.com>
2024-05-14 12:22:34 -0300

    add show password funcionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-/* HAMBURGER BUTTON */
+//BURGER BUTTON
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -6,3 +6,17 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
+
+//SHOW PASSWORD
+document.querySelector("#show-password").addEventListener("click", () => {
+    const input = document.querySelector("#register-password");
+    const button = document.querySelector("#show-password");
+    if (input.getAttribute("type") === "password") {
+        input.setAttribute("type", "text");
+        button.innerHTML = "Hide password";
+    }
+    else {
+        input.setAttribute("type", "password");
+        button.innerHTML = "Show password";
+    }
+})
