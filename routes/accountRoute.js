31a77414b699afd2e955dccc1997f471f8bcf1a9
ndamatta <damatta.natanael@gmail.com>
2024-05-16 21:48:42 -0300

    add route to post method

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,8 @@ const controller = require("../controllers/accountController")
 //Routes
 router.get('/login', utilities.handleErrors(controller.buildLogin));
 router.get('/register', utilities.handleErrors(controller.buildRegistrer));
+router.post('/register', utilities.handleErrors(controller.registerAccount));
+
 
 module.exports = router;
 
