9ad842dd4c48461b2c432e1633bec2a8863eee7d
ndamatta <damatta.natanael@gmail.com>
2024-05-16 21:48:26 -0300

    add check if element is in page

--- public/js/script.js
+++ public/js/script.js
@@ -8,15 +8,18 @@ const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
 //SHOW PASSWORD
-document.querySelector("#show-password").addEventListener("click", () => {
-    const input = document.querySelector("#register-password");
-    const button = document.querySelector("#show-password");
-    if (input.getAttribute("type") === "password") {
-        input.setAttribute("type", "text");
-        button.innerHTML = "Hide password";
-    }
-    else {
-        input.setAttribute("type", "password");
-        button.innerHTML = "Show password";
-    }
-})
+const passwordElement = document.querySelector("#show-password");
+if (passwordElement) {
+    passwordElement.addEventListener("click", () => {
+        const input = document.querySelector("#register-password");
+        const button = document.querySelector("#show-password");
+        if (input.getAttribute("type") === "password") {
+            input.setAttribute("type", "text");
+            button.innerHTML = "Hide password";
+        }
+        else {
+            input.setAttribute("type", "password");
+            button.innerHTML = "Show password";
+        }
+    })
+}
