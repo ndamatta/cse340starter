dd4e4b89f7c9e47c9a44b0f9941e634645de7f96
ndamatta <damatta.natanael@gmail.com>
2024-05-16 21:48:00 -0300

    add register account

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const utilities = require('../utilities/')
+const accountModel = require("../models/account-model")
 
 const accountController = {}
 
@@ -19,6 +20,39 @@ accountController.buildRegistrer = async function (req, res, next) {
       nav,
       grid,
     })
+  }
+accountController.registerAccount = async function (req, res) {
+    let nav = await utilities.getNav()
+    const grid = await utilities.buildRegister();
+    const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+    console.log(req.body)
+    const regResult = await accountModel.registerAccount(
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_password
+    )
+  
+    if (regResult) {
+      console.log("FIRST NAME",account_firstname)
+      req.flash(
+        "notice",
+        `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+      )
+      res.status(201).render("./account/login", {
+        title: "Login",
+        nav,
+        grid,
+      })
+    } else {
+      req.flash("notice", "Sorry, the registration failed.")
+      res.status(501).render("./account/register", {
+        title: "Registration",
+        nav,
+        grid,
+      })
+    }
   }
 
 module.exports = accountController
