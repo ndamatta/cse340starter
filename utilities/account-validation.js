630a0d085a90aea97a9f0cb0bcfc783fc0339989
ndamatta <damatta.natanael@gmail.com>
2024-05-17 11:10:58 -0300

    add custom checker to email

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,3 +1,4 @@
+const accountModel = require("../models/account-model")
 const utilities = require(".")
   const { body, validationResult } = require("express-validator")
   const validate = {}
@@ -13,12 +14,17 @@ const utilities = require(".")
         .withMessage("Please provide a first name."), // on error this message is sent.
   
       // lastname is required and must be string
-      body("account_lastname")
-        .trim()
-        .escape()
-        .notEmpty()
-        .isLength({ min: 2 })
-        .withMessage("Please provide a last name."), // on error this message is sent.
+      body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail() // refer to validator.js docs
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+          throw new Error("Email exists. Please log in or use different email")
+        }
+      }), // on error this message is sent.
   
       // valid email is required and cannot already exist in the DB
       body("account_email")
@@ -43,6 +49,32 @@ const utilities = require(".")
         .withMessage("Password does not meet requirements."),
     ]
   }
+  validate.loginRules = () => {
+    return [
+      body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail() // refer to validator.js docs
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+          throw new Error("Email exists. Please log in or use different email")
+        }
+      }),
+      body("account_password")
+        .trim()
+        .notEmpty()
+        .isStrongPassword({
+          minLength: 12,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        })
+        .withMessage("Password does not meet requirements."),
+    ]
+  }
 
   validate.checkRegData = async (req, res, next) => {
     const { account_firstname, account_lastname, account_email } = req.body
@@ -50,7 +82,6 @@ const utilities = require(".")
     errors = validationResult(req)
     if (!errors.isEmpty()) {
       let nav = await utilities.getNav()
-      const grid = await utilities.buildRegister();
       res.render("./account/register", {
         errors,
         title: "Registration",
@@ -58,7 +89,23 @@ const utilities = require(".")
         account_firstname,
         account_lastname,
         account_email,
-        grid,
+      })
+      return
+    }
+    next()
+  }
+
+  validate.checkLoginData = async(req, res, next) => {
+    const account_email = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      let nav = await utilities.getNav()
+      res.render("./account/login", {
+        errors,
+        title: "Registration",
+        nav,
+        account_email,
       })
       return
     }
