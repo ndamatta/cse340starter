b3eba156e8ae958252c6c774ff7a01fdcc3abecf
ndamatta <damatta.natanael@gmail.com>
2024-05-18 20:49:14 -0300

    add label to dropdown

--- utilities/index.js
+++ utilities/index.js
@@ -66,8 +66,8 @@ Util.buildDetailGrid = async function(data) {
 
 Util.buildClassificationList = async function (classification_id = null) {
   let data = await invModel.getClassifications()
-  let classificationList =
-    '<select name="classification_id" id="classificationList" required>'
+  let classificationList = '<label class="add-inventory-label">Classification'
+  classificationList += '<select name="classification_id" id="classificationList" required>'
   classificationList += "<option value=''>Choose a Classification</option>"
   data.rows.forEach((row) => {
     classificationList += '<option value="' + row.classification_id + '"'
@@ -80,6 +80,7 @@ Util.buildClassificationList = async function (classification_id = null) {
     classificationList += ">" + row.classification_name + "</option>"
   })
   classificationList += "</select>"
+  classificationList += "</label>"
   return classificationList
 }
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
