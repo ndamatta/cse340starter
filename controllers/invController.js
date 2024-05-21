2b36d3ef0b5bfce7cc8caa0017c5116bff7dcbec
ndamatta <damatta.natanael@gmail.com>
2024-05-21 16:55:24 -0300

    add buildEditInventory

--- controllers/invController.js
+++ controllers/invController.js
@@ -123,4 +123,28 @@ invCont.getInventoryJSON = async (req, res, next) => {
     next(new Error("No data returned"))
   }
 }
+invCont.buildEditInventory = async function (req, res, next) {
+  const inventoryId = parseInt(req.params.inventoryId);
+  let nav = await utilities.getNav()
+  const itemData = await invModel.getInventoryByInventoryId(inventoryId)
+  const classificationList = await utilities.buildClassificationList(itemData.classification_id)
+  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
+  res.render('./inventory/edit-inventory', {
+    title: `Edit ${itemName}`,
+    nav,
+    classificationList,
+    errors: null,
+    inv_id: itemData.inv_id,
+    inv_make: itemData.inv_make,
+    inv_model: itemData.inv_model,
+    inv_year: itemData.inv_year,
+    inv_description: itemData.inv_description,
+    inv_image: itemData.inv_image,
+    inv_thumbnail: itemData.inv_thumbnail,
+    inv_price: itemData.inv_price,
+    inv_miles: itemData.inv_miles,
+    inv_color: itemData.inv_color,
+    classification_id: itemData.classification_id
+  })
+}
 module.exports = invCont
