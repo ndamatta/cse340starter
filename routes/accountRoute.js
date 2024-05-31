588f7a8f91972eacac38d4afd2f6b948653500e9
ndamatta <damatta.natanael@gmail.com>
2024-05-31 11:26:16 -0300

    add account validations for access in management

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -35,23 +35,33 @@ router.post("/update-account-password",
   utilities.handleErrors(controller.updatePassword)
 )
 router.get("/management",
-utilities.checkJWTToken,
+ utilities.checkJWTToken,
  utilities.checkLogin,
  utilities.checkAccountAccess,
  utilities.handleErrors(controller.buildAccountManagement))
-router.get('/management/update/:accountId', utilities.handleErrors(controller.buildEditAccount));
+router.get('/management/update/:accountId',
+ utilities.checkJWTToken,
+ utilities.checkLogin,
+ utilities.checkAccountAccess,
+ utilities.handleErrors(controller.buildEditAccount)
+ );
 router.post("/management/update-account",
-accountValidate.updateBasicInfoRules(),
-accountValidate.checkBasicInfoData,
-utilities.handleErrors(controller.updateAccount)
+ accountValidate.updateBasicInfoRules(),
+ accountValidate.checkBasicInfoData,
+ utilities.handleErrors(controller.updateAccount)
 )
 router.post("/management/update-account-password",
  accountValidate.updatePasswordRules(),
  accountValidate.checkPasswordData,
  utilities.handleErrors(controller.updatePassword)
 )
-router.get('/management/delete/:accountId', utilities.handleErrors(controller.buildDeleteAccount));
-router.post('/management/delete-account', utilities.handleErrors(controller.deleteAccount));
+router.get('/management/delete/:accountId',
+ utilities.checkJWTToken,
+ utilities.checkLogin,
+ utilities.checkAccountAccess, 
+ utilities.handleErrors(controller.buildDeleteAccount));
+router.post('/management/delete-account', utilities.handleErrors(controller.deleteAccount)
+);
 
 router.get("/logout", utilities.handleErrors(controller.accountLogout))
 
